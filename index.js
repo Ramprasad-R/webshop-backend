42f73aae9f8b496db2c3789856d3453ea2b6bae8
Ramprasad-R <ramprasad.info@gmail.com>
2020-10-22 22:40:39 +0200

    updated

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 import dotenv from 'dotenv'
 dotenv.config()
+import path from 'path'
 import express from 'express'
 import colors from 'colors'
+import morgan from 'morgan'
 import connectDB from './config/db.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
-import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+import uploadRoutes from './routes/uploadRoutes.js'
 
 connectDB()
 
 const app = express()
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
 
 app.use(express.json())
 
@@ -26,9 +34,14 @@ app.use('/api/users', userRoutes)
 
 app.use('/api/orders', orderRoutes)
 
+app.use('/api/upload', uploadRoutes)
+
 app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 )
+
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 app.use(notFound)
 app.use(errorHandler)
